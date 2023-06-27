c61b771b3b1d4778e8d0770cfb0324c776d20004
Sensaz <klaudiusz.wiktor1@gmail.com>
2023-06-27 18:52:43 +0200

    Added button component

--- app/components/button.js
+++ app/components/button.js
@@ -1,10 +1,15 @@
 import Component from '@glimmer/component';
 
 export default class ButtonComponent extends Component {
-  checkThemeClass(theme) {
-    return theme === 'dark' ? 'button--dark' : 'button';
+  checkButtonClass(theme, imgOuter) {
+    const checkTheme = theme === 'dark' ? 'button button--dark' : 'button';
+
+    const checkImgIsOuter = imgOuter ? 'button--outer' : '';
+
+    return `${checkImgIsOuter} ${checkTheme}`;
   }
+
   checkImageClass(imgOuter) {
-    return imgOuter ? 'button__img--outer' : 'button__img';
+    return imgOuter ? 'button__img button__img--outer' : 'button__img';
   }
 }
